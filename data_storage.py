22c4dbbaff73ab99685140dafa95a9b8bc35df10
V <v@Vs-MacBook-Pro.local>
2022-04-22 19:09:39 -0700

    finished the scraper except for a few final touches

--- data_storage.py
+++ data_storage.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 @dataclass
-class LinkedinProfile():
+class LinkedinProfile:
     first_name: str
     last_name: str
     location: str
@@ -9,5 +9,11 @@ class LinkedinProfile():
     profile_pic: str # stored as link to the source image
 
 class LinkedinEmployees(list):
-    def add_employee(self, first_name:str, last_name:str, /, *, location:str, label:str, profile_pic:str) -> None:
+    def add_employee(self, first_name:str, last_name:str, *, location:str, label:str, profile_pic:str) -> None:
         self.append(LinkedinProfile(first_name, last_name, location, label, profile_pic))
+    
+    def save_as_csv(self, file_name: str):
+        with open(file_name, 'w') as f:
+            f.write('first_name,last_name,location,label,profile_pic\n')
+            for employee in self:
+                f.write(f'{employee.first_name},{employee.last_name},{employee.location},{employee.label},{employee.profile_pic}\n')
--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@ import os, time
 import undetected_chromedriver.v2 as uc
 # selenium imports
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 # other imports
@@ -24,15 +26,11 @@ class LinkedinScraper():
         using a fake login because my actual one is through 
         apple, requiring 2-factor auth every time
         '''
-        # find the email and password fields
-        email_field = self.driver.find_element(By.ID, 'session_key')
-        password_field = self.driver.find_element(By.ID, 'session_password')
-        # enter the email and password
-        email_field.send_keys(self.email)
-        password_field.send_keys(self.password)
-        # find & click the login button
-        submit_btn = self.driver.find_element(By.CLASS_NAME, "sign-in-form__submit-button")
-        submit_btn.click()
+        # type in email and passsword
+        self.driver.find_element(By.ID, 'session_key').send_keys(self.email)
+        self.driver.find_element(By.ID, 'session_password').send_keys(self.password)
+        # find & click the sign-in button
+        submit_btn = self.driver.find_element(By.CLASS_NAME, "sign-in-form__submit-button").click()
     
     def extract_company_employees(self, company_url: str='https://www.linkedin.com/company/hubspot/') -> None:
         # go to the company page (the people section specifically)
@@ -47,37 +45,48 @@ class LinkedinScraper():
         people_list = self.driver.find_elements(By.XPATH, '//div[@class="scaffold-finite-scroll__content"]/ul/li')
         # keep track of which person you're on
         counter = 0
-        # keep track of original tab
-        original_tab = self.driver.current_window_handle
-        for _ in range(5):
-            unseen_employees = people_list[counter:]
-            employee = unseen_employees[counter]
-            # get the name of the employee, only public employees have this class name
-            try:
-                name = employee.find_element(By.CLASS_NAME, 'org-people-profile-card__profile-title').text
-            except:
-                # we don't care about the exception, this just lets us know that we're on a private profile
-                print('nope')
-                counter += 1
-                continue
-            # click on their profile for location & profile pic
-            employee.click()
-            # wait for page to load
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'pb2')))
-            # get location
-            location = self.driver.find_element(By.CLASS_NAME, 'pb2').text
-            # get profile pic
-            profile_pic = self.driver.find_element(By.CLASS_NAME, 'pv-top-card-profile-picture__image').get_attribute('src')
-            print(f'{name=} - {location=} - {profile_pic=}')
-            # go back to the page of all the employees
-            self.driver.back()
-            # update counter to start further in the list
+        while len(self.employees) < 10:
+            employee = people_list[counter]
+            # get the name & label for each employee
+            name, label, *_ = employee.text.split('\n')
+            # skip if their profile is private
+            if not name.startswith('LinkedIn'):
+                # open their profile in a new tab
+                ActionChains(self.driver).key_down(Keys.COMMAND).click(employee).perform()
+                # go to the second tab
+                self.driver.switch_to.window(self.driver.window_handles[-1])
+                # wait for page to load
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'pb2')))
+                # get location
+                location = self.driver.find_element(By.CLASS_NAME, 'pb2').text
+                # get profile pic
+                try:
+                    profile_pic = self.driver.find_element(By.CLASS_NAME, 'pv-top-card-profile-picture__image').get_attribute('src')
+                except:
+                    # dont care about the exception, this just lets us know that there is no profile pic
+                    profile_pic = ''
+                # close the tab to back to the original tab
+                if len(self.driver.window_handles) > 1:
+                    self.driver.close()
+                # switch to original window so selenium can continue
+                self.driver.switch_to.window(self.driver.window_handles[0])
+                # add info to employees
+                if profile_pic:
+                    self.employees.add_employee(*name.split(maxsplit=1), location=location, label=label, profile_pic=profile_pic)
+                # update counter to start further in the list
             counter += 1
-            if counter == len(unseen_employees):
+            if counter == len(people_list):
                 # Scroll to bottom of page to load more employees
                 self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-                # same line as above, could outsource into a function if needed
-                people_list = self.driver.find_elements(By.XPATH, '//div[@class="scaffold-finite-scroll__content"]/ul/li')
+                old_len = len(people_list)
+                # check for new people 5 times before giving up
+                loop_count = 0
+                while loop_count < 5 and old_len == len(people_list):
+                    loop_count += 1
+                    time.sleep(0.5)
+                    # same line as above, could outsource into a function if needed
+                    people_list = self.driver.find_elements(By.XPATH, '//div[@class="scaffold-finite-scroll__content"]/ul/li')
+            # print(self.employees)
     
     def _driver_setup(self) -> uc.Chrome:
         # Configure basic chrome options 
@@ -95,3 +104,4 @@ if __name__ == '__main__':
     scraper = LinkedinScraper()
     scraper.login_from_home()
     scraper.extract_company_employees()
+    scraper.employees.save_as_csv('test_run1.csv')
