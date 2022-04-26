ea3c7739a48377dd603812a6c6b6cbd67286f5a2
V <v@Vs-MacBook-Pro.local>
2022-04-25 17:22:16 -0700

    Slowed down all the processes and added a little bit of random movement. Hopefully this will help a little to alleviate the pressure from anti bot stuff

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
+
+# get contents from readme
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name = 'leegs',
-    version = '1.1.0',
+    version = '1.2.0',
     packages = ['leegs'],
+    author = "Vidith Balasa",
+    author_email = "vidithbalasa@gmail.com",
+    description="A LinkedIn Profile Scraper",
     entry_points = {
         'console_scripts': [
             'leegs = leegs.__main__:main'
@@ -15,5 +23,9 @@ setup(
         'pandas',
         'numpy',
         'tqdm',
-    ]
+    ],
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/vidithbalasa/Divercity-Challenge",
+    python_requires=">=3.10"
 )
